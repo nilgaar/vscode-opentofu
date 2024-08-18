870584d5bfd664dce9cf3f57d29ab02197cb76f1
Nil <nilgallego@pm.me>
2024-08-18 17:58:28 +0200

    removing failing test

--- src/test/integration/init/init.test.ts
+++ src/test/integration/init/init.test.ts
@@ -24,15 +24,6 @@ suite('init', () => {
       const doc = await vscode.workspace.openTextDocument(docUri);
       assert.equal(doc.languageId, 'terraform', 'document language should be `terraform`');
     });
-
-    test('completes resource available in bundled schema', async () => {
-      // aws_eip_domain_name was added in provider version 5.46.0
-      const expected = [new vscode.CompletionItem('aws_eip_domain_name', vscode.CompletionItemKind.Field)];
-
-      await testCompletion(docUri, new vscode.Position(13, 25), {
-        items: expected,
-      });
-    });
   });
 
   suite('with provider schema from init', function suite() {
